b3c61a4ab54af2a9dce6a0c7c4085ff469be8e96
mmrbulbul <mmrbulbul@gmail.com>
2024-09-02 10:47:39 +0900

    feat: mutual information

---
+++ datum/eda/feature_importance.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.feature_selection import mutual_info_regression
+
+def make_mi_scores(X, y, discrete_features):
+    """ref :https://www.kaggle.com/code/ryanholbrook/mutual-information
+
+    """
+    mi_scores = mutual_info_regression(X, y, discrete_features=discrete_features)
+    mi_scores = pd.Series(mi_scores, name="MI Scores", index=X.columns)
+    mi_scores = mi_scores.sort_values(ascending=False)
+    return mi_scores
+
+def plot_mi_scores(scores):
+    scores = scores.sort_values(ascending=True)
+    width = np.arange(len(scores))
+    ticks = list(scores.index)
+    plt.barh(width, scores)
+    plt.yticks(width, ticks)
+    plt.title("Mutual Information Scores")
+
